4da162bcbf0421049bf2d9ef83a74f175a72b8ce
MihirSutariya <sutariya.mihirkumar@iitgn.ac.in>
2023-11-01 14:33:40 +0530

    Home page is complete

---
+++ src/Banner.js
@@ -0,0 +1,16 @@
+function Banner() {
+    return (
+        <div class="FirstDiv">
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/dace47b4-a5cb-4368-80fe-c26f3e77d540/f5b52435-458f-498f-9d1d-ccd4f1af9913/IN-en-20231023-popsignuptwoweeks-perspective_alpha_website_small.jpg" className="FirstImg"/>
+        <div class="ColorShade"></div>
+        <div class="WelcomeDiv">
+          <div className="Wlc">Welcome!</div>
+          
+          <div className="Text">Enjoy big movies, hit series and more from &#x20B9;149</div>
+          <button className="Member btn">Start Your Membership</button>
+        </div>
+      </div> 
+    );
+  }
+  
+  export default Banner;
